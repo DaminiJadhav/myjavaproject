8cf5b48615ddef9c44af67a191967324fa653591
DaminiJadhav <daminijadhav1213@gmail.com>
2026-09-07 12:07:28 +0530

    test CI failure

--- src/main/java/com/java/myproject/myproject/controller/TestController.java
+++ src/main/java/com/java/myproject/myproject/controller/TestController.java
@@ -3,16 +3,24 @@ package com.java.myproject.myproject.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/test")
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping
     public String test() {
         return "Hello World CI/CD!!!";
     }
 
-    @GetMapping("/test/welcome")
+    @GetMapping("/welcome")
     public String testWelcome() {
         return "Welcome to CI/CD!!!";
+    }
+
+    @GetMapping("/addition")
+    public Integer addition() {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        return c;
     }
 }
